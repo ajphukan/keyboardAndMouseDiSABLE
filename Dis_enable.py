54603b7951ae0c30bd8f80496fe5d108b2a412f6
Ankur Jyoti Phukan <leocreed1994@gmail.com>
2015-11-21 10:40:51 -0800

    Create Dis_enable.py
    To disable the linux system keyboard and mouse .
    Requirement:
    xinput command .
    
    Application :
    Server based client locking system

---
+++ Dis_enable.py
@@ -0,0 +1,25 @@
+import os
+import time
+list_id=[]
+id = os.popen('xinput |grep -iv "master"|grep -v "core"|grep -P -o "id=[0-9]+"|grep -P -o "[0-9]+"')
+for i in id:
+	list_id.append(i)
+
+
+
+def lockSystem():
+	for i in list_id:
+		os.system("xinput disable " + i )
+
+def unlockSystem():
+	for i in list_id:
+		os.system("xinput enable " + i)
+
+####an example for test run	
+
+lockSystem()
+print "System Locked"
+time.sleep(10)
+unlockSystem()
+print "System Unlocked"
+#######
